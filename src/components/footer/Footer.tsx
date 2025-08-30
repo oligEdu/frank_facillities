ece7dd444ad3b938662a43127c7272b70d2406fe
user <user@users-MacBook-Pro.local>
2025-08-30 18:05:18 -0300

    base do site

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,31 @@
+import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react';
+function Footer() {
+
+    const data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-nav-footer text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            RH GENERATION - Grupo 04 | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                 <LinkedinLogoIcon size={48} weight='bold' />
+                </a>
+                <a href="https://github.com/Grupo-4-Turma-Javascript-07" target="_blank" rel="noopener noreferrer">
+                 <GithubLogoIcon size={48} weight='bold' />
+                </a>
+                <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                 <InstagramLogoIcon size={48} weight='bold' />
+                </a>    
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,31 @@
+import RH from "../../assets/RH.svg"
+
+function Home() {
+    return (
+        <>
+            <div id='home' className=" min-h-screen bg-background flex justify-center scroll-mt-20">
+                <div className='container grid grid-cols-2  text-nav-footer'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4 px-9 ">
+                        <h2 className='text-4xl font-bold text-nav-footer'>
+                            RH Generation
+                        </h2>
+                        <p className='text-lg'>
+                            Gerencie sua equipe com mais eficiência, organização e praticidade.
+                            Aqui você encontra tudo o que precisa para administrar dados de colaboradores, acompanhar desempenho, controlar benefícios e muito mais, tudo em um só lugar.
+                        </p>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src={RH}
+                            alt="Imagem Página Home"
+                            className='w-3/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
